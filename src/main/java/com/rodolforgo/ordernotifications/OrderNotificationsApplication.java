cf791a39c705e25ebe4402a3ef5df5a1267a040d
José Rodolfo <rdfvalerio@gmail.com>
2026-05-25 15:45:18 -0300

    chore: scaffold inicial do projeto Spring Boot

---
+++ src/main/java/com/rodolforgo/ordernotifications/OrderNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.rodolforgo.ordernotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodolforgo/ordernotifications/OrderNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolforgo.ordernotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
