6aa778c30745d63690921387cc5c16a3d025adb2
José Rodolfo <rdfvalerio@gmail.com>
2026-05-25 16:30:07 -0300

    chore: configura propriedades do Kafka e habilita scheduling

--- src/main/java/com/rodolforgo/ordernotifications/OrderNotificationsApplication.java
+++ src/main/java/com/rodolforgo/ordernotifications/OrderNotificationsApplication.java
@@ -2,8 +2,10 @@ package com.rodolforgo.ordernotifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OrderNotificationsApplication {
 
     public static void main(String[] args) {
