9a2cbfc2918e77b8a1cdf6e605e797205da9eb4d
José Rodolfo <rdfvalerio@gmail.com>
2026-05-25 17:53:36 -0300

    feat: torna status opcional com default CREATED

--- src/main/java/com/rodolforgo/ordernotifications/controller/OrderController.java
+++ src/main/java/com/rodolforgo/ordernotifications/controller/OrderController.java
@@ -20,14 +20,22 @@ public class OrderController {
     @PostMapping("/sync")
     @ResponseStatus(HttpStatus.CREATED)
     public List<Order> createSync(@RequestBody List<Order> orders) {
-        orders.forEach(producer::sendSync);
-        return orders;
+        List<Order> withDefaults = applyDefaultStatus(orders);
+        withDefaults.forEach(producer::sendSync);
+        return withDefaults;
     }
 
     @PostMapping("/async")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public List<Order> createAsync(@RequestBody List<Order> orders) {
-        orders.forEach(producer::send);
-        return orders;
+        List<Order> withDefaults = applyDefaultStatus(orders);
+        withDefaults.forEach(producer::send);
+        return withDefaults;
+    }
+
+    private List<Order> applyDefaultStatus(List<Order> orders) {
+        return orders.stream()
+            .map(o -> new Order(o.id(), o.item(), o.status() != null ? o.status() : "CREATED"))
+            .toList();
     }
 }
